8fe67ea8eba77384b3cd7bd63a7b8842d2c74e14
Baraa Kayed <braaadel78@gmail.com>
2024-03-09 22:28:38 +0200

    Process Monitor & File Manager

---
+++ Bash Script/Process Monitor System/Config_Editing.sh
@@ -0,0 +1,249 @@
+#!/bin/bash
+
+config_file="Config_sets.txt"
+
+# Function to update configuration values in the file
+update_configuration() {
+    local config_file="$1"
+    local var_name="$2"
+    local new_value="$3"
+    
+    sed -i "s/^$var_name=.*/$var_name=$new_value/" "$config_file"
+
+
+	# Delete the original file
+	rm "$config_file"
+
+	# Create a new file with updated content
+	echo "# Configuration variables" >> "$config_file"
+	echo "MEM_MIN=$MEM_MIN" >> "$config_file"
+	echo "MEM_MAX=$MEM_MAX" >> "$config_file"
+	echo "CPU_MIN=$CPU_MIN" >> "$config_file"
+	echo "CPU_MAX=$CPU_MAX" >> "$config_file"
+	echo "REFRESH_TIME=$REFRESH_TIME" >> "$config_file"
+	echo "CPU_ALERT_THRESHOLD=$CPU_ALERT_THRESHOLD" >> "$config_file"
+	echo "MEMORY_ALERT_THRESHOLD=$MEMORY_ALERT_THRESHOLD" >> "$config_file"
+}
+
+
+
+# Function to change configuration with input validation
+Change_Config() {
+    local option=$1
+    ##Set default values
+    local new_REFRESH_TIME=$REFRESH_TIME
+    local new_CPU_MIN=$CPU_MIN
+    local new_CPU_MAX=$CPU_MAX
+    local new_MEM_MIN=$MEM_MIN
+    local new_MEM_MAX=$MEM_MAX
+    local new_CPU_ALERT_THRESHOLD=$CPU_ALERT_THRESHOLD
+    local new_MEMORY_ALERT_THRESHOLD=$MEMORY_ALERT_THRESHOLD
+
+    case $option in
+        A)
+            read -p "Enter new refresh rate in seconds: " new_REFRESH_TIME
+            ;;
+        B)
+            read -p "Enter new minimum CPU usage limit: " new_CPU_MIN
+            ;;
+        C)
+            read -p "Enter new maximum CPU usage limit: " new_CPU_MAX
+            ;;
+        D)
+            read -p "Enter new minimum memory usage limit: " new_MEM_MIN
+            ;;
+        E)
+            read -p "Enter new maximum memory usage limit: " new_MEM_MAX
+            ;;
+        F)
+            read -p "Enter new Cpu Alert usage threshold: " new_CPU_ALERT_THRESHOLD
+            ;;
+        G)
+            read -p "Enter new Memory Alert usage threshold: " new_MEMORY_ALERT_THRESHOLD
+            ;;
+        *)
+            echo "Invalid option."
+            return
+            ;;
+    esac
+
+    # Validate input
+    local valid=true
+    local error_message=""
+
+    if ! [[ "$new_REFRESH_TIME" =~ ^[1-9][0-9]*$ ]]; then
+        valid=false
+        error_message+="Refresh rate must be a positive integer.\n"
+    fi
+
+    if (( $(bc <<< "$new_CPU_MIN < 0 || $new_CPU_MIN > 100" 2>/dev/null) )); then
+        valid=false
+        error_message+="Minimum CPU usage limit must be between 0 and 100.\n"
+    fi
+
+    if (( $(bc <<< "$new_CPU_MAX < 0 || $new_CPU_MAX > 100" 2>/dev/null) )); then
+        valid=false
+        error_message+="Maximum CPU usage limit must be between 0 and 100.\n"
+    fi
+
+    if (( $(bc <<< "$new_MEM_MIN < 0 || $new_MEM_MIN > 100" 2>/dev/null) )); then
+        valid=false
+        error_message+="Minimum memory usage limit must be between 0 and 100.\n"
+    fi
+
+    if (( $(bc <<< "$new_MEM_MAX < 0 || $new_MEM_MAX > 100" 2>/dev/null) )); then
+        valid=false
+        error_message+="Maximum memory usage limit must be between 0 and 100.\n"
+    fi
+
+    if (( $(echo "$new_CPU_MIN >= $new_CPU_MAX" | bc -l) )); then
+        valid=false
+        error_message+="Minimum CPU usage limit must be smaller than maximum CPU usage limit.\n"
+    fi
+
+    if (( $(echo "$new_MEM_MIN >= $new_MEM_MAX" | bc -l) )); then
+        valid=false
+        error_message+="Minimum memory usage limit must be smaller than maximum memory usage limit.\n"
+    fi
+
+    if ! $valid; then
+        echo -e "Changes not applied. Please correct the following errors:\n$error_message"
+        return
+    fi
+
+    # Apply changes
+    case $option in
+        A)
+         REFRESH_TIME=$new_REFRESH_TIME
+         log_configuration_change "Refresh rate changed to $new_REFRESH_TIME seconds" 
+         update_configuration "$config_file" "$REFRESH_TIME" "$new_REFRESH_TIME"
+         ;;
+
+        B) 
+        CPU_MIN=$new_CPU_MIN 
+        log_configuration_change "Minimum CPU usage limit changed to $new_CPU_MIN%"
+        update_configuration "$config_file" "$CPU_MIN" "$new_CPU_MIN"
+        ;;
+        
+        C) 
+        CPU_MAX=$new_CPU_MAX 
+        log_configuration_change "Maximum CPU usage limit changed to $new_CPU_MAX%"
+        update_configuration "$config_file" "$CPU_MAX" "$new_CPU_MAX"
+        ;;
+        
+        D) 
+        MEM_MIN=$new_MEM_MIN 
+        log_configuration_change "Minimum memory usage limit changed to $new_MEM_MIN%"
+        update_configuration "$config_file" "$MEM_MIN" "$new_MEM_MIN"
+        ;;
+        
+        E) 
+        MEM_MAX=$new_MEM_MAX
+        log_configuration_change "Maximum memory usage limit changed to $new_MEM_MAX%"
+        update_configuration "$config_file" "$MEM_MAX" "$new_MEM_MAX"
+        ;;
+        
+        F) 
+        CPU_ALERT_THRESHOLD=$new_CPU_ALERT_THRESHOLD
+        log_configuration_change "Cpu Alert usage threshold changed to $new_CPU_ALERT_THRESHOLD%"
+        update_configuration "$config_file" "$CPU_ALERT_THRESHOLD" "$new_CPU_ALERT_THRESHOLD"
+        ;;
+        
+        G) 
+        MEMORY_ALERT_THRESHOLD=$new_MEMORY_ALERT_THRESHOLD
+        log_configuration_change "Memory Alert usage threshold changed to $new_MEMORY_ALERT_THRESHOLD%"
+        update_configuration "$config_file" "$MEMORY_ALERT_THRESHOLD" "$new_MEMORY_ALERT_THRESHOLD"
+        ;;
+        
+    esac
+    echo -e "\n Changes applied successfully. \n"
+}
+
+
+Config_Print(){
+        echo  "Refresh Rate Display System statistics $REFRESH_TIME sec."
+        echo  "Minimum Cpu Usage Filtration--> $CPU_MIN% ."
+        echo  "Maximum Cpu Usage Filtration--> $CPU_MAX% ."
+        echo  "Minimum Memory Usage Filtration--> $MEM_MIN% ."
+        echo  "Maximum Memory Usage Filtration--> $MEM_MAX% ."
+        echo -e "Cpu Alert usage threshold--> $CPU_ALERT_THRESHOLD% ."
+        echo -e "Memory Alert usage threshold--> $MEMORY_ALERT_THRESHOLD% .\n\n"
+
+}
+
+Config_Print_Animation() {
+    clear  # Clear the screen
+
+    # Define animation frames
+    frames=("|    " "/    " "-    " "\\    ")
+
+    # Print animation frames
+    for ((i = 0; i < 2; i++)); do
+        for frame in "${frames[@]}"; do
+            printf "\r%s [frame] Refresh Rate Display System statistics $REFRESH_TIME sec." "$frame"
+            sleep 0.1  # Adjust sleep duration for animation speed
+        done
+    done
+
+    # Print configuration information in a table format
+    printf "\r%-10s %-10s %-10s %-10s %-10s\n" "Parameter" "Min" "Max" "Condition" "Result"
+    printf "%-10s %-10s %-10s %-10s %-10s\n" "CPU Usage" "$CPU_MIN%" "$CPU_MAX%" "0 <= CPU_MIN < CPU_MAX <= 100" ""
+    printf "%-10s %-10s %-10s %-10s %-10s\n" "Memory Usage" "$MEM_MIN%" "$MEM_MAX%" "0 <= MEM_MIN < MEM_MAX <= 100" ""
+    
+    printf "\n\n\r%s Refresh Rate Display System statistics $REFRESH_TIME sec."
+    printf "\n\n\r%s Cpu Alert usage threshold $CPU_ALERT_THRESHOLD %."
+    printf "\n\n\r%s Memory Alert usage threshold $MEMORY_ALERT_THRESHOLD %."
+    echo -e "\n\n"
+}
+
+
+
+Config_Set(){
+	while true; do
+	Config_Print
+	 echo "Choose Config you want change"
+	 echo "1. Refresh Rate Display System statistics in sec."
+	 echo "2. Cpu Usage Min Limits"
+	 echo "3. Cpu Usage Max Limits"
+	 echo "4. Memory Min Usage Limits"
+	 echo "5. Memory Max Usage Limits"
+	 echo "6. Cpu Alert usage threshold"
+	 echo "7. Memory Alert usage threshold"
+	 echo "8. Back to Main"
+	 read -p "Enter your choice: " choice_search_filter
+	 echo -e "\n"
+	 
+	 case $choice_search_filter in
+          1) Change_Config "A" ;;
+          2) Change_Config "B" ;;
+	  3) Change_Config "C" ;;
+	  4) Change_Config "D" ;;
+	  5) Change_Config "E";;
+	  6) Change_Config "F";;
+	  7) Change_Config "G";;
+          8) echo "Exiting."; break ;; 
+	  *) echo -e "Invalid choice. Please try again.\n\n" ;;
+    	esac
+    	
+	done
+}
+
+Configuration(){
+
+	while true; do
+	 echo "Configuration Options"
+	 echo "1. Change Configurations"
+	 echo "2. Display Configurations"
+	 echo "3.Back to Main"
+	 read -p "Enter your choice: " choice_search_filter
+	 echo -e "\n"
+	
+	case $choice_search_filter in
+	  1) Config_Set;;
+	  2) Config_Print_Animation;;
+          3) echo "Exiting."; break ;; 
+	  *) echo -e "Invalid choice. Please try again.\n\n" ;;
+    	esac
+    done
+}
+
---
+++ Bash Script/Process Monitor System/Monitor_Processes.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Function to display process information
+display_processes() {
+    clear  # Clear the screen
+    printf "%-10s %-10s %-10s %-10s %-10s %s\n" "USER" "PPID" "PID" "CPU %" "MEM %"
+        Option_here="$1"
+        User_search_filter="$2"
+        alert=0
+    for pid_dir in /proc/[0-9]*; do
+        pid=$(basename "$pid_dir")
+        ##Assign all pids in array for further usage
+        all_pids+=("$pid")
+        ppid=$(awk '/PPid:/ {print $2}' "$pid_dir/status" 2>/dev/null)
+        ##checks if the variable ppid is not empty,process directory does not correspond to a running process
+        if [ -n "$ppid" ]; then
+            cpu=$(Get_Cpu_Usage "$pid")
+            mem=$(Get_Mem_Usage "$pid_dir")
+            if (( $(echo "$mem < 0.10" | bc) )); then
+              continue
+            fi
+            command=$(tr -d '\0' < "$pid_dir/cmdline" 2>/dev/null)
+            # Get the username associated with the process
+            proc_user=$(awk '$1 == "Uid:" {print $2}' "$pid_dir/status" 2>/dev/null)
+            # Convert UID to username
+            proc_username=$(getent passwd "$proc_user" | cut -d: -f1)
+
+		if (( $(echo "$mem > $MEMORY_ALERT_THRESHOLD" | bc -l) )); then
+		alert=50
+		fi
+		if (( $(echo "$cpu > $CPU_ALERT_THRESHOLD" | bc -l) ));then
+		alert=20
+		fi
+		
+            # Option B: Print only for user of $USER_SEARCH
+            if [ "$Option_here" = "B" ]; then
+		if [ "$proc_username" != "$User_search_filter" ]; then
+		##Mismatch username
+                    continue
+		fi
+            fi
+            # Option C: Print only for process memory usage between ($MEM_MIN & $MEM_MAX)
+            if [ "$1" = "C" ]; then
+                if (( $(echo "$mem < $MEM_MIN || $mem > $MEM_MAX" | bc -l) )); then
+                    continue
+                fi
+            fi
+            # Option D: Print only for process cpu usage between ($CPU_MIN & $CPU_MAX)
+            if [ "$1" = "D" ]; then
+                if (( $cpu < $CPU_MIN || $cpu > $CPU_MAX )); then
+                    continue
+                fi
+            fi
+            
+            printf "%-10s %-10s %-10s %-10s %-10s %s\n" "$proc_username" "$ppid" "$pid" "$cpu" "$mem"
+            if [ "$alert" = "20" ]; then
+		 echo -e "\n\n $pid PROCESS ALERT EXCEDDED CPU ALERT THRESHOLD \n\n"
+		fi
+	   if [ "$alert" = "50" ]; then
+		 echo -e "\n\n $pid PROCESS ALERT EXCEDDED MEM ALERT THRESHOLD \n\n"
+		fi
+
+        fi
+    done
+       if [ "$Option_here" = "A" ]; then
+        sleep REFRESH_TIME  # Wait for 15 seconds before refreshing
+        display_processes
+       fi
+}
+
